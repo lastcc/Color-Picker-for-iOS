892695b3c0c4e6aa5520380557de01b7c02185f2
cheng chen <tony@xmind.org>
2018-04-16 12:06:11 +0800

    a

---
+++ HRColorPicker/HRColorPickerX/HRColorPickerX.h
@@ -0,0 +1,18 @@
+//
+//  HRColorPickerX.h
+//  HRColorPickerX
+//
+//  Created by 陈成 on 2018/4/16.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HRColorPickerX.
+FOUNDATION_EXPORT double HRColorPickerXVersionNumber;
+
+//! Project version string for HRColorPickerX.
+FOUNDATION_EXPORT const unsigned char HRColorPickerXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HRColorPickerX/PublicHeader.h>
+
+
